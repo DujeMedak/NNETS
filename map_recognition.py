5fc9c2065214d12ce96b2c07872096ced844571c
dbartolovic@gmail.com <dbartolovic@gmail.com>
2018-11-17 10:41:23 +0100

    Added a test script to generate marked input image

--- map_recognition.py
+++ map_recognition.py
@@ -1,13 +1,16 @@
 from keras_preprocessing.image import ImageDataGenerator
-from PIL import Image
+import numpy as np
+import model_helper
+from PIL import Image, ImageDraw, ImageFont
 import os
 import math
 
 
+##################LOADING THE IMAGE#############################################
+
 def divide_and_conquer(map_file_path, output_map_sections_path,
                        target_section_width = 128,
                        target_section_height = 128,
-                       test_batchsize = 1,
                        class_subfolder = 'unclassified',
                        sections_file_format = 'png'):
     
@@ -44,13 +47,19 @@ def divide_and_conquer(map_file_path, output_map_sections_path,
                         sections_file_format)
 
 
+
+def load_sections(output_map_sections_path,
+                  test_batchsize = 1,
+                  target_section_width = 128,
+                  target_section_height = 128):
+
     tf_image_data_gen = ImageDataGenerator()
 
     tf_image_data = tf_image_data_gen.flow_from_directory(
         output_map_sections_path,
         target_size = (target_section_height, target_section_width),
         class_mode = None,
-        batch_size = 1,
+        batch_size = test_batchsize,
         shuffle = False
     )
 
@@ -72,7 +81,93 @@ def calc_intersection_offset(img_dim_size, section_dim_size):
         return last_section_pos/(n_sections - 1)
 
 
+##########################NET PREDICTION########################################
+
+def get_predictions(img_data, model_file_path, weights_file_path):
+
+    model = model_helper.load_model_from_file(model_file_path)
+    model.load_weights(weights_file_path)
+
+    print(img_data.samples)
+
+    # Get the predictions from the model using the generator
+    predictions = model.predict_generator(img_data,
+                                        steps=img_data.samples / img_data.batch_size, verbose=1)
+
+    return predictions
+
+
+###########################DISPLAY PREDICTIONS##################################
+
+
+def get_box_from_index(index, full_width, full_height,
+                       target_section_width = 128,
+                       target_section_height = 128):
+
+    n_sections_width =                                                          \
+        n_sections_in_dim(full_width, target_section_width)
+    
+    offset_width =                                                              \
+        calc_intersection_offset(full_width, target_section_width)
+    offset_height =                                                             \
+        calc_intersection_offset(full_height, target_section_height)
+    
+    index_row = int(index/n_sections_width)
+    index_col = index%n_sections_width
+
+    return  (index_col*offset_width, index_row*offset_height,
+             index_col*offset_width + target_section_width,
+             index_row*offset_height + target_section_height)
+
+
+def mark_prediction(map_file_path, marked_map_file_path, predictions,
+                    target_section_width = 128,
+                    target_section_height = 128):
+
+    map_image = Image.open(map_file_path)
+    map_draw = ImageDraw.Draw(map_image)
+
+    n_sections = predictions.shape[0]
+    n_classes = predictions.shape[1]
+
+    for index in range(n_sections):
+        box = get_box_from_index(index, map_image.width, map_image.height,
+                                 target_section_width=target_section_width,
+                                 target_section_height=target_section_height)
+        box_center = ((box[2] + box[0])/2, (box[3] + box[1])/2)
+
+        map_draw.text(box_center, index_to_class(np.argmax(predictions[index])))
+
+    map_image.save(marked_map_file_path)
+
+
+def index_to_class(index):
+    classes = ['Airport', 'Beach', 'Bridge', 'Center', 'Church',
+               'DenseResidential', 'Farmland', 'Forest', 'Industrial',
+               'Meadow', 'MediumResidential', 'Mountain', 'Park', 'Parking',
+               'Playground', 'Pond', 'Port', 'RailwayStation', 'River',
+               'School', 'SparseResidential', 'Square', 'Stadium',
+               'StorageTanks', 'Viaduct']
+    
+    return classes[index]
+
+def class_to_index(neural_class):
+    classes = ['Airport', 'Beach', 'Bridge', 'Center', 'Church',
+               'DenseResidential', 'Farmland', 'Forest', 'Industrial',
+               'Meadow', 'MediumResidential', 'Mountain', 'Park', 'Parking',
+               'Playground', 'Pond', 'Port', 'RailwayStation', 'River',
+               'School', 'SparseResidential', 'Square', 'Stadium',
+               'StorageTanks', 'Viaduct']
+    
+    for i in range(len(classes)):
+        if classes[i] == neural_class:
+            return i
+
 
 #example usage:
 
 #divide_and_conquer('./source/source.png', './destination')
+#gen_img_data = load_sections('./destination')
+#pred = get_predictions(gen_img_data, './net_data/model_arh2.json', './net_data/model_w_102.h5')
+
+#mark_prediction('./source/source.png', pred)
---
+++ mr_test.py
@@ -0,0 +1,19 @@
+import map_recognition as mr
+import numpy as np
+
+
+mr.divide_and_conquer('./source/source.png', './destination')
+gen_img_data = mr.load_sections('./destination')
+#gen_img_data = mr.load_sections('./testground')
+pred = mr.get_predictions(gen_img_data, './net_data/model_arh2.json', './net_data/model_w_102.h5')
+
+#print(mr.index_to_class(np.argmax(pred[0])))
+
+
+#total = 0
+#for i in range(pred.shape[0]):
+#    if pred[i][mr.class_to_index('Airport')] > 0.5:
+#        total = total + 1
+#print(total)
+
+mr.mark_prediction('./source/source.png', './source/marked.png', pred)
